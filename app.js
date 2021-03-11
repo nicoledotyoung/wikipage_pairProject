16f8d233b6ec57033097df1b0e878a8ef0d309f8
nicole.young <nicoledyoung94@gmail.com>
2021-03-11 11:56:32 -0500

    models/index.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const path = require("path");
+const { db } = require("./models");
+
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
@@ -14,4 +16,8 @@ app.listen(3000, (err) => {
   if (!err) {
     console.log("Listening at lh:3000");
   }
+});
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
 });
